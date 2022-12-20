a604d71badb6bcfff5afef5903b69eea51dc3fae
kitamura <s.kitamura.biz@gmail.com>
2022-12-20 19:18:22 +0900

    置換コマンドの作成。置換ケースの追加。ライセンス情報追加。

--- extension.js
+++ extension.js
@@ -1,30 +1,50 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "repcase" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
+	/**
+	 * Example
+	 */
 	let disposable = vscode.commands.registerCommand('repcase.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Repcase!');
 	});
 
+	/**
+	 * Replace cases
+	 */
+	let replaceCommonToAsset = vscode.commands.registerCommand('repcase.commonToAsset', function(){
+		replaceAll(/\"[.\/]*?(common\/.*?)\"/, '\"{{ asset(\'$1\') }}\"');
+	});
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(replaceCommonToAsset);
+}
+
+/**
+ * 指定された又はアクティブなエディタの文字を正規表現で置き換えます。
+ * @param {RegExp} pattern 置換対象を正規表現で指定します。
+ * @param {string} replacement patternに一致したパターンがこの文字列に置換されます。
+ * @param editor 置換対象のvscode editorオブジェクトを指定します。
+ */
+function replaceAll(pattern, replacement, editor = null){
+	if(editor == null){
+		editor = vscode.window.activeTextEditor;
+	}
+
+	new RegExp(pattern,'g');
+	if(editor) {
+		const document = editor.document;
+		var text = document.getText();
+		var textReplaced = text.replace(pattern, replacement);
+
+		const range = new vscode.Range(0, 0, editor.document.lineCount + 1, 0);
+		editor.edit(editBuilder => {
+			editBuilder.replace(range, textReplaced);
+		});
+	}
 }
 
 // This method is called when your extension is deactivated
