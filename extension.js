bd39181bc483e5ee7a37c65456ebed9879403136
kitamura <s.kitamura.biz@gmail.com>
2023-01-31 19:28:53 +0900

    href内に書かれたサイト内相対リンクを route記法に置換するコマンドを追加

--- extension.js
+++ extension.js
@@ -16,11 +16,16 @@ function activate(context) {
 	 * Replace cases
 	 */
 	let replaceCommonToAsset = vscode.commands.registerCommand('repcase.commonToAsset', function(){
-		replaceAll(/\"[.\/]*?(common\/.*?)\"/, '\"{{ asset(\'$1\') }}\"');
+		replaceAllEditor(/\"[.\/]*?(common\/.*?)\"/, '\"{{ asset(\'$1\') }}\"');
+	});
+
+	let replacePathToRoute = vscode.commands.registerCommand('repcase.pathToRoute', function(){
+		replaceRoute();
 	});
 
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(replaceCommonToAsset);
+	context.subscriptions.push(replacePathToRoute);
 }
 
 /**
@@ -29,7 +34,7 @@ function activate(context) {
  * @param {string} replacement patternに一致したパターンがこの文字列に置換されます。
  * @param editor 置換対象のvscode editorオブジェクトを指定します。
  */
-function replaceAll(pattern, replacement, editor = null){
+function replaceAllEditor(pattern, replacement, editor = null){
 	if(editor == null){
 		editor = vscode.window.activeTextEditor;
 	}
@@ -44,6 +49,97 @@ function replaceAll(pattern, replacement, editor = null){
 		editor.edit(editBuilder => {
 			editBuilder.replace(range, textReplaced);
 		});
+	}
+}
+
+function replaceRoute(editor = null){
+	if(editor == null){
+		editor = vscode.window.activeTextEditor;
+	}
+
+	if(editor) {
+		const document = editor.document;
+		var text = document.getText();
+		var matches = Array.from(text.matchAll(/<a[\s\S]*?>/g));
+		var arrayMatches = JSON.parse(JSON.stringify(matches));
+
+		var currentUrls = Array();
+		var debug = Array();
+
+		var regFromMiddleBracket = new RegExp(/^\{/) // { で始まる
+
+		/**
+		 * href内のURLを取得
+		 */
+		arrayMatches.forEach(function(value, index){
+			var href = value[0].match(/href="(.*?)"/);
+
+			if(href != null){
+				var currentUrl = href[1];
+				debug.push(currentUrl);
+				if(currentUrl != '#' && currentUrl.indexOf(/http.*/)!==0 && currentUrl.indexOf(/\/\/.*/)!==0 && !regFromMiddleBracket.test(currentUrl)){
+					currentUrls.push(currentUrl);
+				}
+			}
+		});
+
+		// 重複削除
+		currentUrls = Array.from(new Set(currentUrls));
+		vscode.window.showInformationMessage(JSON.stringify(debug));
+
+		/**
+		 * buddies {before: '置換前', after: '置換後}
+		 */
+		var buddies = Array();
+
+		currentUrls.forEach(function(url, index){
+			var paths = url.split('/');
+			var trimmedPath = Array();
+			paths.forEach(function(path, pathIndex){
+
+				// 空でも、相対指定でもない
+				var regFromDots = new RegExp(/^\.+/) // n個の.から始まる
+				if(path != "" && !regFromDots.test(path)){
+								// debug.push(path);
+					trimmedPath.push(path);
+				}
+			});
+			/**
+			 * パスをroute記法に変換
+			 */
+			var tail = trimmedPath.pop();
+			var regHtmlFile = new RegExp(/\.html$/) // .html で終わる
+			if(regHtmlFile.test(tail)){
+				var tailPaths = tail.split('.');
+				// htmlを削除して返す
+				tailPaths.pop();
+				trimmedPath.push(tailPaths[0]);
+			}else{
+				// スラ止めの場合は indexを付与
+				debug.push(tail);
+				trimmedPath.push(tail);
+				trimmedPath.push('index');
+			}
+
+			var route = trimmedPath.join('.');
+			route = '{{route(\''+route+'\')}}';
+			buddies.push({before: url, after: route});
+		});
+
+
+		// 置換
+		var text = document.getText();
+
+		const range = new vscode.Range(0, 0, editor.document.lineCount + 1, 0);
+		buddies.forEach(function(value, index){
+			text = text.replaceAll(value.before, value.after);
+		});
+
+		editor.edit(editBuilder => {
+			editBuilder.replace(range, text);
+		});
+
+		// vscode.window.showInformationMessage(JSON.stringify(buddies));
 	}
 }
 
